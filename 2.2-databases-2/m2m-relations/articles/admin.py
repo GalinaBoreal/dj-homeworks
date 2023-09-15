6afcbd390c0475a94469ff2dcf76f261586b99b9
Galina <galina_ver@mail.ru>
2023-09-15 16:00:40 +0500

    the second task on databases-2 is ready

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,45 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        flag = False
+        count_main = 0
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            data = form.cleaned_data
+            if data.get('is_main'):
+                flag = True
+                count_main += 1
+            else:
+                continue
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+
+        if flag is not False or count_main > 1:
+            raise ValidationError('Назначьте один основной тэг')
+
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title']
+    inlines = [ScopeInline, ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
